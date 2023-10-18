9cad2617dd82ff71f15230f198f95f169be1d97a
GabrielCarvalho <gabriel.carvalho@huawei.com>
2023-10-18 15:24:49 -0300

    test(TestClass): Adding the iniciail classes for testing the program

---
+++ PlatformService.Tests/Controllers/PlatformsControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using PlatformService;
+
+namespace PlatformService.Tests.Controllers
+{
+    //[TestClass]
+    //public class PlatformsControllerTests
+    //{
+    //    private readonly HttpClient _httpClient;
+
+        //public PlatformsControllerTests()
+        //{
+        //    var webAppFactory = new WebApplicationFactory<Program>();
+        //    _httpClient = webAppFactory.CreateDefaultClient();
+        //}
+
+        //[TestMethod]
+        //public async Task DefaultGetAllRoute_ReturnsIEnumerablePlatforms()
+        //{
+        //    var response = await _httpClient.GetAsync(requestUri:"api/Platforms");
+        //    var stringResult = await response.Content.ReadAsStringAsync();
+        //    Console.WriteLine(stringResult);
+            // Console.WriteLine(typeof(stringResult));
+            // Assert.AreEqual("1");
+    //    }
+    //}    
+}
+
---
+++ PlatformService.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PlatformService.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
