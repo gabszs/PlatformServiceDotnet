ce9cb1c2e95ee2c26ba5f198375dfd14f86c50e8
GabrielCarvalho <gabriel.carvalho@huawei.com>
2023-10-18 15:27:48 -0300

    feat(DbRepository): Adding the platform rp alongside with it owns dbcontexto and interfaces and platform model

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async void CreatePlatform(Platform plat)
+        {
+            if (plat == null)
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+
+            await _context.Platforms.AddAsync(plat);
+        }
+
+        public async Task<IEnumerable<Platform>> GetAllPlatforms()
+        {
+            return await _context.Platforms.ToListAsync();
+        }
+
+        public async Task<Platform?> GetPlatformById(int id)
+        {
+            return await _context.Platforms.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<bool> SaveChanges()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Publisher { get; set; }
+
+        public required string Cost { get; set; }
+// D:\dotnet\microservices\PlatformService/PlatformService.csproj
+    }
+}
